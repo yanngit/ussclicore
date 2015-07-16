d90d1bad18b65b54168da6f7781d7509f495e721
Joris Bremond <joris.bremond@usharesoft.com>
2015-07-16 10:07:39 +0200

    Add UShareSoft cli core module source

---
+++ src/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup,find_packages
+
+
+# Declare your packages' dependencies here, for eg:
+requires=['cmd2==0.6.7',
+            'argparse',
+         ]
+    
+                    
+setup (  
+
+  
+  install_requires=requires,
+  
+  name = 'ussclicore',
+  version = 1.0,
+  description='UShareSoft cli core module',
+  #long_description='',
+  packages = find_packages(),
+  author = 'Joris Bremond',
+  author_email = 'joris.bremond@usharesoft.com',
+  license="Apache License 2.0",
+  #url = '',
+  classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+       
+  
+  #long_description= 'Long description of the package',
+  
+)
