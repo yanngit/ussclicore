918568524f1c5ef7264fee76597e8a88b6e2427f
Joris Bremond <joris.bremond@usharesoft.com>
2015-07-30 15:11:02 +0200

    Fix bug in the ascii bar graph

--- src/ussclicore/utils/ascii_bar_graph.py
+++ src/ussclicore/utils/ascii_bar_graph.py
@@ -1,7 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 __author__="UShareSoft"
 
 def print_graph(values):
@@ -10,16 +6,15 @@ def print_graph(values):
                 if len(v)>max:
                         max=len(v)
         for v in values:
+                value = int(values[v])
                 if len(v)<max:
                         newV=v+(" " * int(max-len(v)))
-                        if values[v]!=-1:
-                                print newV, values[v]*'|'+'-'*int(50-values[v])
+                        if value!=-1:
+                                print newV, value*'|'+'-'*int(50-value)
                         else:
                                 print newV,20*'-'+"UNLIMITED"+21*'-'
                 else:
-                        if values[v]!=-1:
-                                print v, values[v]*'|'+'-'*int(50-values[v])
+                        if value!=-1:
+                                print v, value*'|'+'-'*int(50-value)
                         else:
                                 print v,20*'-'+"UNLIMITED"+21*'-'
-                        
-                        
